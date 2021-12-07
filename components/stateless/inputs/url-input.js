27eb0d62c684660b777f8d3582185ab7179398ee
Vinson Ong <vinson1493@gmail.com>
2021-12-07 22:51:00 +0800

    Added type=url to url-input

--- components/stateless/inputs/url-input.js
+++ components/stateless/inputs/url-input.js
@@ -13,6 +13,7 @@ const UrlInput = ({ name, control, rules, error, ...props }) => {
           {...field}
           error={error ? true : false}
           helperText={error?.message}
+          inputProps={{ type: "url" }}
           InputLabelProps={{ shrink: true }}
           {...props}
         />
